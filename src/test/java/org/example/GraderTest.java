bc2739a9587ad90b804feb9fb5592b5a6b315ac5
MaximSilva <58977585+MaximSilva@users.noreply.github.com>
2023-10-07 11:01:00 +0300

    Class Grader
    Determines the grade by JUnit

---
+++ src/test/java/org/example/GraderTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraderTest {
+
+    @Test
+    void fiftyNineShouldReturnF(){
+        var grader = new Grader();
+        assertEquals('F', grader.determineLetterGrade(59));
+    }
+
+}
