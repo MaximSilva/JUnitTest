0a0fc310d917737e6619c5f4295a5d59dccf997c
MaximSilva <58977585+MaximSilva@users.noreply.github.com>
2023-10-07 10:53:47 +0300

    New Method
    New method scenarios for verification

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -5,10 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-@Test
-    void twoPlusTwoShouldEqualFour(){
+    @Test
+    void twoPlusTwoShouldEqualFour() {
     var calculator = new Calculator();
     assertEquals(4, calculator.add(2, 2));
-
+    assertTrue(calculator.add(2, 2) == 4);
 }
+    @Test
+    void threePlusSevenShouldEqualTen () {
+        var calculator = new Calculator();
+        assertEquals(10, calculator.add(3, 7));
+        }
 }
